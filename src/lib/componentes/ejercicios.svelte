b41bff25ffeda807c162abcf808f9d47bb2391c8
MrChileball <chileball5@gmail.com>
2023-10-31 14:37:39 -0300

    1

---
+++ src/lib/componentes/ejercicios.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let a = 3;
+    export let b = 3;
+    export let c = 3;
+
+    $: perim = a + b + c;
+    $: semiPerim = (a+b+c)/2;
+    $: area = Math.sqrt(semiPerim*(semiPerim-a)*(semiPerim-b)*(semiPerim-c));
+</script>
+
+<div>
+    <div>
+        <h2>Calculadora Perimetro</h2>
+        <h3>Ingresa datos</h3>
+        
+        {#if a+b > c && b+c > a && a+c > b }
+            <p>Resultado: {perim}</p>
+            <p>Resultado: {semiPerim}</p>
+            <p>Resultado: {area}</p>
+            
+        {:else}
+            <h3>Error!:</h3><p> Para que un triangulo exista la suma de dos lados debe ser mayor al tercero</p>
+        {/if}
+        
+    </div>
+    <div>
+        <input type="number" placeholder="Variable 1" bind:value={a}>
+        <input type="number" placeholder="Variable 2" bind:value={b}>
+        <input type="number" placeholder="Variable 3" bind:value={c}>
+    </div>
+    <div>
+
+    </div>
+
+</div>
+
+
+
+<style>
+
+
+</style>
---
+++ src/lib/componentes/navbar.svelte
@@ -0,0 +1,62 @@
+<script>
+    //Permite que al cargar el componente desde una página puedes cambiar el titulo 
+    export let title = "test";
+
+</script>
+
+
+<div class="title"> 
+    <h1>{title}</h1>
+</div>
+<ul class="flex center navbar ">
+    <li>
+        <a>
+            <h3>holis</h3>
+        </a>
+    </li>
+    <li>
+        <a>
+            <h3>holis</h3>
+        </a>
+    </li>
+    <li>
+        <a>
+            <h3>holis</h3>
+        </a>
+    </li>
+    <li>
+        <a>
+            <h3>holis</h3>
+        </a>
+    </li>
+
+</ul>
+
+
+<style>
+    li{
+        margin: 0px;
+        padding: 0px;
+        transition: all ease-in 0.3s;
+    }
+    li:hover{
+        background-color: #558564ff;
+    }
+    .navbar{
+        background-color: #a4969bff;
+        width: 80%;
+        border-radius: 15px;
+        
+        
+    }
+    .flex{
+        display: flex;
+    }
+    .center{
+        flex-direction: row;
+        align-items: center;
+        align-content: center;
+        justify-content: center;
+    }
+
+</style>
